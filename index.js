efefcb0030c882936d3e6cac7882ec333306a60b
jcthekin1234 <93554396+jcthekin12345@users.noreply.github.com>
2025-06-22 15:30:31 +0200

    Refactor display update logic and reset kicking goal on reset

--- index.js
+++ index.js
@@ -14,28 +14,32 @@ const kickStr = document.getElementById("kicking-str");
 xpText.innerHTML = `XP: ${user.kicking_exp}`;
 kickStr.innerHTML = `Str: ${user.str}`;
 
-kick.addEventListener("click", () => {
-    upgrade();
-    user.kicking_exp += 1;
-    xpText.innerHTML = `XP: ${user.kicking_exp}`;
-    kickStr.innerHTML = `Str: ${user.str}`;
-});
-
-rst.addEventListener("click", () => {
-    user.kicking_exp = 0;
-    user.str = 0;
-    xpText.innerHTML = `XP: ${user.kicking_exp}`;
-    kickStr.innerHTML = `Str: ${user.str}`;
-});
-
 let kicking_exp_goal = 10;
 let kicking_exp_goal_multiplier = 2;
 
 function upgrade() {
     if (user.kicking_exp >= kicking_exp_goal) {
         user.str += 1;
-        kickStr.innerHTML = `Str: ${user.str}`;
         user.kicking_exp = 0;
         kicking_exp_goal *= kicking_exp_goal_multiplier;
+        updateDisplay();
     }
 }
+
+function updateDisplay() {
+    xpText.innerHTML = `XP: ${user.kicking_exp}`;
+    kickStr.innerHTML = `Str: ${user.str}`;
+}
+
+kick.addEventListener("click", () => {
+    user.kicking_exp += 1;
+    updateDisplay();
+    upgrade();
+});
+
+rst.addEventListener("click", () => {
+    user.kicking_exp = 0;
+    user.str = 0;
+    kicking_exp_goal = 10;
+    updateDisplay();
+});
